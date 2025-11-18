9b08d90939c6caba71ea7de8ac1a5e3c8ab5cb2e
nawra-alaradi <nawraaradi@gmail.com>
2025-11-18 20:36:18 +0300

    improved login page

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -16,12 +16,13 @@ function LoginPage()  {
 
   return (
     <div className={style.LoginContainerPage}>
-    <div className="flex flex-col items-center justify-center">
-    <CiDeliveryTruck className='icon-size' color="white"/>
-
-{/* <BsTruck size={200} color="white"/>
- */}    <div className='mt-30'>
-      <button onClick={handleLogin} className="flex items-center gap-2">
+          <img src={Logo} alt="Logo" className="mt-10 ml-5 w-20  h-20 absolute" />
+    <div className="flex flex-col items-center justify-center h-full">
+  
+    <CiDeliveryTruck className='w-2/3 h-1/4 text-white' />
+    <p className='text-white'>OrderTrack</p>
+   <div className='mt-5'>
+      <button onClick={handleLogin} className="flex items-center gap-2 w-75 h-15 justify-center bg-white text-blue-700 font-bold py-2 px-4 rounded hover:bg-blue-100">
 <svg
 stroke="currentColor"
 fill="currentColor"
@@ -33,9 +34,13 @@ xmlns="http://www.w3.org/2000/svg"
 >
 <path d="M7.462 0H0v7.19h7.462zM16 0H8.538v7.19H16zM7.462 8.211H0V16h7.462zm8.538 0H8.538V16H16z"></path>
 </svg>
-Login with Single Sign On
+LOGIN WITH SSO
 </button>
     </div>
+    <footer className="mt-10 text-white flex flex-col items-center justify-center ">
+    <p>  © {new Date().getFullYear()} BAPCO ENERGIES ID&T</p>
+
+    </footer>
     </div>
 
  
